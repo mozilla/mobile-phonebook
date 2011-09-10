ed4f6304c3064d91fe8d7c1ce507bb9e0afb3b13
Atul Varma <varmaa@gmail.com>
2011-09-10 08:47:39 -0700

    thumbnails are now fetched using a thread pool, and we always try to fetch thumbnails for people that don't have photos.

--- fetch.py
+++ fetch.py
@@ -2,6 +2,8 @@ import os
 import sys
 import json
 import urllib2
+import Queue
+from threading import Thread
 
 MY_DIR = os.path.abspath(os.path.dirname(__file__))
 STATIC_FILES_DIR = os.path.join(MY_DIR, 'static-files')
@@ -9,8 +11,28 @@ JSON_FEED_FILENAME = os.path.join(MY_DIR, 'people.json')
 THUMBNAIL_DIR = os.path.join(STATIC_FILES_DIR, 'images', 'people')
 
 BASE_URL = 'https://ldap.mozilla.org/phonebook'
+NO_PHOTO_THUMBNAIL_SIZE = 3302
 JSON_URL = '%s/directory.php' % BASE_URL
 THUMBNAIL_URL = '%s/pic.php?type=thumb&mail=' % BASE_URL
+
+def fetch_thumbnails(emails):
+    def worker():
+        while True:
+            try:
+                email = queue.get(True, 0.1)
+            except Queue.Empty:
+                return
+            maybe_fetch_thumbnail(email)
+            queue.task_done()
+
+    NUM_WORKER_THREADS = 5
+
+    queue = Queue.Queue()
+    for email in emails:
+        queue.put(email)
+    for i in range(NUM_WORKER_THREADS):
+        Thread(target=worker).start()
+    queue.join()
 
 def install_auth_handler(username, password):
     pw_manager = urllib2.HTTPPasswordMgrWithDefaultRealm()
@@ -35,19 +57,19 @@ def maybe_fetch_json_feed():
 def maybe_fetch_thumbnail(email):
     thumbnail = os.path.join(THUMBNAIL_DIR, '%s.jpg' % email)
     if not os.path.exists(thumbnail):
-        sys.stdout.write("Fetching thumbnail for %s... " % email)
-        sys.stdout.flush()
-        try:
-            url = THUMBNAIL_URL + email
-            response = urllib2.urlopen(url).read()
-            f = open(thumbnail, 'wb')
-            f.write(response)
-            f.close()
-            print "OK."
-        except urllib2.HTTPError, e:
-            if e.code != 404:
-                raise
-            print "Not Found."
+        return
+    if os.stat(thumbnail).st_size != NO_PHOTO_THUMBNAIL_SIZE:
+        return
+    print "Fetching thumbnail for %s..." % email
+    try:
+        url = THUMBNAIL_URL + email
+        response = urllib2.urlopen(url).read()
+        f = open(thumbnail, 'wb')
+        f.write(response)
+        f.close()
+    except urllib2.HTTPError, e:
+        if e.code != 404:
+            raise
 
 if __name__ == '__main__':
     config = json.load(open('config.json', 'r'))
@@ -59,5 +81,4 @@ if __name__ == '__main__':
     if not os.path.exists(THUMBNAIL_DIR):
         os.mkdir(THUMBNAIL_DIR)
 
-    for email, info in people.items():
-        maybe_fetch_thumbnail(email)
+    fetch_thumbnails([email for email in people])
